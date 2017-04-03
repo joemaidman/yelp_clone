fd590e2128a21dbbd3b737d6efb2c3f7db38824b
Bernard Malhame <bernard.malhame@gmail.com>
2017-04-03 16:02:06 +0100

    creating routes

--- spec/features/restaurant_feature_spec.rb
+++ spec/features/restaurant_feature_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 feature 'restaurants' do
   context 'no restaurants have been added' do
-    scenario 'sjould display a prompt to add a restaurant' do
+    scenario 'should display a prompt to add a restaurant' do
       visit '/restaurants'
       expect(page).to have_content 'No restaurants yet'
       expect(page).to have_link "Add a restaurant"
