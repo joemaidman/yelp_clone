fb14699f3cadc1d99d0aa7682fd26abce716d17a
joemaidman <joemaidman@gmail.com>
2017-04-04 19:41:38 +0100

    Adds reviews model and validation to restaurant name, unique and review score

---
+++ spec/models/restaurant_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe Restaurant, type: :model do
+  it 'is not valid with a name of less than three characters' do
+    restaurant = Restaurant.new(name: "kf")
+    expect(restaurant).to have(1).error_on(:name)
+    expect(restaurant).not_to be_valid
+  end
+
+  it "is not valid unless it has a unique name" do
+    Restaurant.create(name: "Joe's Tavern")
+    restaurant = Restaurant.new(name: "Joe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
+
+end
